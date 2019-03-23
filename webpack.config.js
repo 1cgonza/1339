ebb5e3718a1d2a9efda74d568dea834f4c7e7d81
Juan Camilo González <info@juancgonzalez.com>
2019-03-22 21:54:31 -0500

    fix image path for webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,16 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader?name=/img/[name].[ext]']
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: 'img/',
+              outputPath: 'img/'
+            }
+          }
+        ]
       }
     ]
   },
